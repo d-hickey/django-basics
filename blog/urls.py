534b385023797f0c5fca44c99ba4ffc3335c7b41
Darragh Hickey <hickeyd1@tcd.ie>
2017-01-21 16:26:38 +0000

    blog creation with model forms

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^user/(?P<user>[a-zA-Z]+)/$', views.user, name="user"),
     url(r'^user/(?P<user>[a-zA-Z]+)/(?P<post_id>[0-9]+)/$', views.post, name="post"),
+    url(r'^create/$', views.create, name="create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponseRedirect as redirect
+from django.urls import reverse
 
-from blog.models import Post
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -12,3 +16,16 @@ def user(request, user):
 
 def post(request, user, post_id):
     return index(request)
+
+def create (request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect(reverse('blog:post', args=(request.user.username, post.id,)))
+    else:
+        form = PostForm()
+        return render(request, "blog/create.html", {'form': form})
